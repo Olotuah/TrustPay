8562772195ea74c481618ccd973760d3d28ae57e
Gbenga Nelson <gbengaolotuah@gmail.com>
2025-07-17 23:13:13 +0100

    Updated register endpoint to use VITE_API_URL for deployment

--- Back/index.js
+++ Back/index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "https://trustpay-8hfr.onrender.com",
+    origin: "https://trust-payy.vercel.app",
     credentials: true,
   })
 );
